5c145d4357fbd9fda786a6477035badae1b301d7
ibnuqoyim <muhamadibnu9@gmail.com>
2018-11-09 05:53:01 +0700

    lm

---
+++ mzdb.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 08, 2018 at 11:49 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.0.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mzdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `telepon` int(11) NOT NULL,
+  `pic` varchar(40) NOT NULL,
+  `kontak_pic` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`id`, `nama`, `alamat`, `telepon`, `pic`, `kontak_pic`) VALUES
+(1, 'PT PLN (Persero)', '-', 22898989, 'Pak Eko', 809898898),
+(2, 'PT Telkom ', '-', 22898989, 'Pak Eka', 8989989);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Client_respon`
+--
+
+CREATE TABLE `Client_respon` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `material_id` int(11) NOT NULL,
+  `isi` varchar(250) NOT NULL,
+  `file_respon` varchar(110) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dok_eng`
+--
+
+CREATE TABLE `dok_eng` (
+  `id` int(11) NOT NULL,
+  `material` int(11) NOT NULL,
+  `file` varchar(100) NOT NULL,
+  `deskripsi` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontrak`
+--
+
+CREATE TABLE `kontrak` (
+  `id` int(11) NOT NULL,
+  `id_material` int(11) NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `deskripsi` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `material`
+--
+
+CREATE TABLE `material` (
+  `id` int(11) NOT NULL,
+  `client` int(11) NOT NULL,
+  `client_respon` varchar(200) DEFAULT NULL,
+  `nama` varchar(100) NOT NULL,
+  `dok_eng` varchar(110) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `permintaan_penawaran` varchar(250) DEFAULT NULL,
+  `pemenang` varchar(110) DEFAULT NULL,
+  `kontrak` varchar(110) DEFAULT NULL,
+  `jadwal_kom` date DEFAULT NULL,
+  `jadwal_pi` date DEFAULT NULL,
+  `ba_inspection` varchar(110) DEFAULT NULL,
+  `irn` int(11) DEFAULT NULL,
+  `jadwal_pengambillan` date DEFAULT NULL,
+  `status_pengambilan` varchar(50) DEFAULT NULL,
+  `hasil_inspeksi_barang` varchar(500) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `create_date` date NOT NULL,
+  `last_update` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `material`
+--
+
+INSERT INTO `material` (`id`, `client`, `client_respon`, `nama`, `dok_eng`, `status`, `permintaan_penawaran`, `pemenang`, `kontrak`, `jadwal_kom`, `jadwal_pi`, `ba_inspection`, `irn`, `jadwal_pengambillan`, `status_pengambilan`, `hasil_inspeksi_barang`, `stok`, `create_date`, `last_update`) VALUES
+(1, 1, NULL, 'Material A', '1', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-04', '2018-11-04'),
+(2, 1, NULL, 'Material B', 'Level4.sql', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-07', '2018-11-07'),
+(8, 2, NULL, 'Material C', 'Mechanical Equipment Datasheet.pdf', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-07', '2018-11-07'),
+(9, 1, NULL, 'Material D', '0', 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(10, 1, '', 'Material E', 'a', 5, '', '', '', '0000-00-00', '0000-00-00', '', 0, '0000-00-00', '', '', 0, '2018-11-08', '2018-11-08'),
+(11, 1, NULL, 'Material F', 'a', 6, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(12, 2, NULL, 'Material G', 'a', 7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(13, 1, NULL, 'Material H', 'a', 8, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(14, 2, NULL, 'Material I', 'a', 9, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(15, 1, NULL, 'Material J', 'a', 10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(16, 2, NULL, 'Material K', 'a', 11, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(17, 1, NULL, 'Material L', 'a', 12, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(18, 2, NULL, 'Material M', 'a', 13, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(19, 1, NULL, 'Material  N', 'N', 14, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(20, 2, NULL, 'Material  O', 'N', 15, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08'),
+(21, 1, NULL, 'Material  P', 'N', 15, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-11-08', '2018-11-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penawaran`
+--
+
+CREATE TABLE `penawaran` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_material` int(11) NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `deskripsi` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `namaStatus` varchar(50) NOT NULL,
+  `keterangan` varchar(110) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id`, `namaStatus`, `keterangan`) VALUES
+(1, 'Waiting for Approval', 'Dokumen Engineering menunggu respon klien'),
+(2, 'Document Approved', 'Dokumen Engineering sudah di approve klien	'),
+(3, 'Document Rejected', 'Dokumen Engineering ditolak klien, sedang diperbaiki engineer	'),
+(4, 'Request a tender', 'Pengadaan telah membuat dokumen permintaan penawaran, menunggu penawaran vendor	'),
+(5, 'Tender', 'Pengumpulan dokumen penawaran dari vendor	'),
+(6, 'Tender Result', '	Pemenang telah dipilih, fase pembuatan kontrak	'),
+(7, 'Kick of Meeting	', '	Fase Kick of Meeting	'),
+(8, 'Production & Inspection', '	Fase Production & Inspection	'),
+(9, 'Result Inspection', 'Berita Acara Inspeksi telah di upload	'),
+(10, 'Repair & Closing', 'Fase Repair & Closing Punch List oleh Vendor	'),
+(11, 'Release IRN', 'Fase penerbitan IRN	'),
+(12, 'Material shipping', 'Fase Pengiriman Material	'),
+(13, 'Material received', 'Material telah diterima Warehouse	'),
+(14, 'Material inspection', 'Material telah di inspeksi Warehouse	'),
+(15, 'Material in Stock', 'Stok Material telah di update	');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(75) NOT NULL,
+  `alamat` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `username` varchar(75) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `enkrip` varchar(50) NOT NULL,
+  `role` varchar(75) NOT NULL,
+  `kodeAsrama` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `alamat`, `email`, `telp`, `username`, `password`, `enkrip`, `role`, `kodeAsrama`) VALUES
+(3, 'Engineering', '-', '-', '085711112228', 'Engineering', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Engineering', ''),
+(4, 'Vendor1', '-', '-', '085711112212', 'Vendor1', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Vendor', ''),
+(5, 'Pengadaan', '-', '-', '085711112216', 'Pengadaan', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Pengadaan', ''),
+(6, 'Vendor2', '-', '-', '085711112220', 'Vendor2', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Vendor', ''),
+(7, 'Client', '-', '-', '085711112224', 'Client', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Client', ''),
+(8, 'Expedeting', '-', '-', '085711112228', 'Expedeting', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Expedeting', ''),
+(9, 'Traffic', '-', '-', '085711112232', 'Traffic', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Traffic', ''),
+(10, 'QC', '-', '-', '085711112236', 'QC', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'QC', ''),
+(11, 'Warehouse', '-', '-', '085711112240', 'Warehouse', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Warehouse', ''),
+(51, 'Muhamad Ibnu Qoyim', 'Bandung', 'muhamadibnu9@gmail.com', '089673569437', 'ibnuqoyim', 'f08dcb43bb4fc72e778d838ea47aadb5', '5a25118b0c3a85.82618145', 'Admin', ''),
+(52, 'Proyek', 'a', 'muhamadibnu9@gmail.com', '089673569437', 'Proyek', 'a5f40f0fe04a6fdc0e423e17cb44e984', '5be05f019290b0.38714493', 'Proyek', 'KP'),
+(53, 'Kepala', 'bdg', 'muhamadibnu9@gmail.com', '089673569437', 'kepalaupt', '7b5243076625e1e0559092e5ae4e66c0', '5a2f9e85c99b71.51626435', 'Kepala UPT Asrama', ''),
+(54, 'Agung Mr.', '0852 72345 1111', '0852 72345 1111', '0852 72345 1111', 'Agung', '7ee8de7fdda016a44d0081ada2e0662a', '5a8ad923c0c659.21245113', 'Operator Lapangan', ''),
+(55, 'Miko Mr.', '0851 7686 2222', '0852 72345 1111', '0851 7686 2222', 'Miko', '54c7a62bdda3b0f339d2b89394d43fb0', '5a8ad956b357b6.51739712', 'Operator Lapangan', ''),
+(56, 'Rindang', '0851 7686 2222', '0852 72345 1111', '0851 7686 2222', 'Rindang', 'bee96cac79d6cf489e9c5970d4b1882e', '5a8ad972a6ced8.96886686', 'Manager', ''),
+(57, 'Nida', '0851 7686 2222', '0852 72345 1111', '0851 7686 2222', 'Nida', '874782e36cc77b3b36016c4879054d58', '5a8ad9849c1880.70704240', 'Administrator', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Client_respon`
+--
+ALTER TABLE `Client_respon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dok_eng`
+--
+ALTER TABLE `dok_eng`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penawaran`
+--
+ALTER TABLE `penawaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `Client_respon`
+--
+ALTER TABLE `Client_respon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dok_eng`
+--
+ALTER TABLE `dok_eng`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `material`
+--
+ALTER TABLE `material`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `penawaran`
+--
+ALTER TABLE `penawaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
