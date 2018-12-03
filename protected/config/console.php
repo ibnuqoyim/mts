f192d8417738e54b36e0effeee1d3799c25e8b64
ibnuqoyim <muhamadibnu9@gmail.com>
2018-12-03 08:43:18 +0700

    kantor

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,37 +1,29 @@
 <?php
-
+// protected/config/console.php
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Console Application',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+	'import'=>array(
+		'application.models.*',
+	),
 
 	// application components
 	'components'=>array(
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	         'class'=>'CDbConnection',
+	         'charset' => 'utf8',
+	         'connectionString'=>'mysql:host=localhost;dbname=mzdb',
+    		 'username'=>'root',
+    		 'password'=>''
 		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-			),
-		),
+                // usefull for generating links in email etc...
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'showScriptName' => FALSE,
+			'rules'=>array(),
+		),			
 	),
 );
